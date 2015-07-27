d2e9f2e24e5b861be0c7908207b58c53a91bb1ea
dking3876 <dking3876@msn.com>
2015-07-27 18:28:15 -0400

    commit

--- admin/BraftonAdminFunctions.php
+++ admin/BraftonAdminFunctions.php
@@ -161,7 +161,7 @@ function print_section_info($args){
             echo '<p>This section controls the general settings for your importer.  Features for this plugin may depend on your settings in this section.  If you need help with your settings you may contact your CMS or visit <a href="http://www.brafton.com/support" target="_blank">Our Support Page</a> for assistance.</p><p>You may also view our pdf <a href="'.$inst.'">Instructions</a>';
         break;
         case 'error':
-            echo '<p>Provides Error Log support.</p>';
+            echo '<p>Provides Error Log support.  Errors resulting in failure to deliver content are directly reported and turn on debug mode capturing all errors for troubleshooting purposes.</p>';
         break;
         case 'article':
             echo '<p>This section is for setting your article specific settings.  All settings on this page are independant of your video settings.';
