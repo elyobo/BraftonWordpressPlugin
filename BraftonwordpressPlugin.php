7a613ce53063e18dcb3f30b0ae3559e698201c88
Liam O'Boyle <liam@elyobo.net>
2018-01-30 17:14:20 +1100

    Support clean HTTPS URLs in BraftonCurlPageURL.
    This keeps the OG metadata cleaner and is consistent with the existing
    behaviour for HTTP with port 80.

--- BraftonwordpressPlugin.php
+++ BraftonwordpressPlugin.php
@@ -330,7 +330,7 @@ EOC;
 
         $pageURL .= "://";
 
-        if ($_SERVER["SERVER_PORT"] != "80")
+        if ($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443")
             $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
         else
             $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
